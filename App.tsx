e31d8a91050bd0dba370349e54a80103a48b2f0b
MichelNassarleb <michelnassar371@gmail.com>
2023-01-03 15:14:08 +0100

    Added signout functionality to the app and implemented it in the home screen

--- App.tsx
+++ App.tsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import { auth } from './config/firebase';
 import { RootNavigator } from './navigation/rootNavigator';
 export default function App() {
-  return <RootNavigator />;
+  const AuthenticatedUserContext = createContext({});
+  const AuthenticatedUserProvider = ({ children }) => {
+    const [user, setUser] = useState<any>(null);
+    return (
+      <AuthenticatedUserContext.Provider value={{ user, setUser }}>
+        {children}
+      </AuthenticatedUserContext.Provider>
+    );
+  };
+  const { user, setUser }: any = useContext(AuthenticatedUserContext);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (authenticatedUser) => {
+      authenticatedUser ? setUser(authenticatedUser) : setUser(null);
+    });
+    unsubscribe();
+    return () => unsubscribe();
+  }, [user]);
+  return (
+    <AuthenticatedUserProvider>
+      <RootNavigator />
+    </AuthenticatedUserProvider>
+  );
 }
--- screens/home/Home.tsx
+++ screens/home/Home.tsx
@@ -3,15 +3,44 @@ import { Text, Alert, View, TouchableOpacity } from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { Entypo } from '@expo/vector-icons';
 import { styles } from './HomeStyles';
+import { auth } from '../../config/firebase';
 export const Home: FC<any> = ({ navigation }) => {
+  const authenticationSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        Alert.alert('Successfully signed out');
+      })
+      .catch(() => {
+        Alert.alert('Please try again.');
+      });
+  };
   useEffect(() => {
     navigation.setOptions({
       headerLeft: () => (
         <TouchableOpacity
           style={styles.loginContainer}
-          onPress={() => navigation.navigate('Login')}
+          onPress={() => {
+            if (auth.currentUser?.email) {
+              Alert.alert('Are you sure you want to sign out?', '', [
+                {
+                  text: 'Cancel',
+                  onPress: () => null,
+                },
+                {
+                  text: 'Sign out',
+                  onPress: () => {
+                    authenticationSignOut();
+                    navigation.navigate('Login');
+                  },
+                },
+              ]);
+            } else navigation.navigate('Login');
+          }}
         >
-          <Text style={{ color: 'white', fontWeight: 'bold' }}>LOGIN</Text>
+          <Text style={{ color: 'white', fontWeight: 'bold' }}>
+            {auth.currentUser?.email ? 'LOGOUT' : 'LOGIN'}
+          </Text>
           <View style={styles.lockContainer}>
             <MaterialIcons name='lock' size={24} color={'orange'} />
           </View>
@@ -27,13 +56,27 @@ export const Home: FC<any> = ({ navigation }) => {
         />
       ),
     });
-  }, [navigation]);
+  }, [navigation, auth]);
 
   return (
     <View style={styles.container}>
       <TouchableOpacity
         style={styles.chatButton}
-        onPress={() => navigation.navigate('Chat')}
+        onPress={() => {
+          if (auth.currentUser?.email) navigation.navigate('Chat');
+          else {
+            Alert.alert('You need to login first', '', [
+              {
+                text: 'Cancel',
+                onPress: () => null,
+              },
+              {
+                text: 'Go to the login page',
+                onPress: () => navigation.navigate('Login'),
+              },
+            ]);
+          }
+        }}
       >
         <Entypo name='chat' size={24} color={'white'} />
       </TouchableOpacity>
