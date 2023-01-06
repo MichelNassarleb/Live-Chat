a94350f1aca13ee3bbb0562da269a73805544482
MichelNassarleb <michelnassar371@gmail.com>
2023-01-06 12:17:22 +0200

    Implemented the API call to update item likes in firebase

--- navigation/rootNavigator.tsx
+++ navigation/rootNavigator.tsx
@@ -1,18 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { onAuthStateChanged } from 'firebase/auth';
 import {
   collection,
-  doc,
-  Firestore,
   getDoc,
   onSnapshot,
+  orderBy,
   query,
 } from 'firebase/firestore';
 import React, { useEffect, useContext, createContext, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { auth, database } from '../config/firebase';
-import { setMemes } from '../redux/slices/RTDBSlice';
-import { RootState } from '../redux/store';
+import { useDispatch } from 'react-redux';
+import { database } from '../config/firebase';
+import { setMemes, updateMemesLikes } from '../redux/slices/RTDBSlice';
 import { ChatStack } from './Stacks/chatStack';
 
 const AuthenticatedUserContext = createContext({});
@@ -26,22 +23,28 @@ const AuthenticatedUserProvider = ({ children }) => {
 };
 
 export const RootNavigator = () => {
-  const memesRef = collection(database, 'memes');
-  const qMemes = query(memesRef);
-  const memes = useSelector((state: RootState) => state.RTDB.memes);
+  const memesRef = collection(database, 'memes', '');
+  const qMemes = query(memesRef, orderBy('createdAt', 'desc'));
   const dispatch = useDispatch();
   useEffect(() => {
     const unsub = onSnapshot(qMemes, (snap) => {
-      console.log(snap.docChanges().length);
       if (snap.docChanges().length > 0) {
         snap.docChanges().map((doc: any) => {
-          dispatch(
-            setMemes({
-              meme: doc.doc.data().meme,
-              likes: [...doc.doc.data().likes],
-              createdAt: doc.doc.data().createdAt.toDate(),
-            })
-          );
+          if (doc.type == 'modified') {
+            dispatch(
+              updateMemesLikes({
+                meme: doc.doc.data().meme,
+                likes: [...doc.doc.data().likes],
+              })
+            );
+          } else
+            dispatch(
+              setMemes({
+                meme: doc.doc.data().meme,
+                likes: [...doc.doc.data().likes],
+                createdAt: doc.doc.data().createdAt.toDate(),
+              })
+            );
         });
       } else {
         snap.docs.map((doc) => {
@@ -50,6 +53,7 @@ export const RootNavigator = () => {
               meme: doc.data().meme,
               likes: [...doc.data().likes],
               createdAt: doc.data().createdAt.toDate(),
+              ...doc,
             })
           );
         });
